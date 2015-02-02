b1e952ecc39e45c2e1b8420d4382cc214a5f940b
Neil Hanlon <neil@neilhanlon.me>
2015-02-02 08:16:37 -0500

    Initial commit

---
+++ readsets.py
@@ -0,0 +1,16 @@
+import ast
+__author__ = 'hanlonn'
+
+sets = open('mediumsets.txt', 'r')
+
+n = sets.readline()
+
+searchSet = {53257: "a", 12546: "b", 83689: "c"}
+
+i = 0
+for set in sets.readlines():
+    a, b, c = set.split(' ')
+    if i in searchSet.keys():
+        res = True if int(eval(searchSet[i])) == 1 else False
+        print "{0} : {1}".format(searchSet[i], res)
+    i += 1
